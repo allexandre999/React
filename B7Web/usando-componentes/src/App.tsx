825b6c3cf4190094a892f0be9ee19498a119ceaf
Joao Alexandre <alexandre_efeta@hotmail.com>
2022-12-29 10:22:01 -0300

    Usando Componentes

---
+++ B7Web/usando-componentes/src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+
+function App(){
+  return (
+    
+    <div>
+      <Header/>
+      Ola Mundo
+    </div>
+  );
+}
+
+export default App;
---
+++ B7Web/usando-componentes/src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header(){
+    return (
+      <header>
+        <h1>Div Cabeçalho</h1>
+        <hr />
+      </header>
+    );
+  }
+
+export default Header;
