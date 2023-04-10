7e6e6506e161a991ab47e381b0d578d7baf5a7f4
Joao Alexandre <alexandre_efeta@hotmail.com>
2023-04-10 09:00:51 -0300

    finalizado subtração da calculadora super simples :P

--- B7Web/CalculadoraBasica/src/App.tsx
+++ B7Web/CalculadoraBasica/src/App.tsx
@@ -1,4 +1,5 @@
 import { Somar } from './components/Somar';
+import { Subtrair } from './components/Subtrair'
 
 function App(){ 
   return(
@@ -6,7 +7,7 @@ function App(){
       <h1>Soma:</h1>
       <Somar/>
       <h1>Subtracao:</h1>
-
+      <Subtrair></Subtrair>
       <h1>Multiplicacao:</h1>
 
       <h1>Divisao:</h1>
--- B7Web/CalculadoraBasica/src/components/Subtrair.tsx
+++ B7Web/CalculadoraBasica/src/components/Subtrair.tsx
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+
 export function Subtrair(){
     const[numbersub1, modificarNumber1] = useState(0);
     const[numberSub2, modificarNumber2] = useState(0);
@@ -6,7 +7,9 @@ export function Subtrair(){
     
     return(
         <div>
-
+            <button onClick={diminuirNum1}>-</button><span>{numbersub1}</span><button onClick={adicionarNum1}>+</button><button onClick={zerarNum1}>Zerar</button><br />
+            <button onClick={diminuirNum2}>-</button><span>{numberSub2}</span><button onClick={adicionarNum2}>+</button><button onClick={zerarNum2}>Zerar</button><br />
+            <button onClick={subtrair}>Subtrair</button><span>Resultado: </span><span>{resultado}</span><button onClick={zerarResultado}>Zerar</button>
         </div>
     );
 
@@ -34,7 +37,11 @@ export function Subtrair(){
         modificarNumber2(0);
     }
 
-    
+    function zerarResultado(){
+        modificarResultado(0);
+    }
 
-
+    function subtrair(){
+        modificarResultado(numbersub1-numberSub2);
+    }
 }
