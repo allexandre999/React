6ea3839247430587088e40597ecea77de8f0aabf
João Alexandre Bezerra da Silva <joao.alexandre@fariasbrito.com.br>
2024-02-02 19:57:08 -0300

    Criando diretório Alura e iniciando projeto de organograma

---
+++ B7Web/greeting-condicional-1/src/App.js
@@ -0,0 +1,12 @@
+import './components/teste.css'
+
+function App() {
+  return (
+    <div className=''>
+       <div className='container gradient-background'>00:00</div>
+       <div className=''>Bom Teste</div>
+    </div>
+  );
+}
+
+export default App;
